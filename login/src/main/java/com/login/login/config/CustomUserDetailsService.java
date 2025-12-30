91b463f4f5d27d8874245862b41734cb4af37d04
Yashavanth Kapila <yashavanthkapila@gmail.com>
2025-12-30 17:16:56 +0530

    branch1 -login and signup authentication

---
+++ login/src/main/java/com/login/login/config/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.login.login.config;
+
+import com.login.login.entity.User;
+import com.login.login.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        System.out.println("🔥 DB lookup for email: " + email);
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
